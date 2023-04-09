f40ced801bd1abe0167d90004d5cd5b0a8e30afa
Mostafa <mostafamibrahim12@gmail.com>
2023-04-09 11:14:11 +0200

    MVP ver1.0

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
@@ -10,10 +9,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        animal = request.form["Question"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=animal,
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
@@ -23,7 +22,7 @@ def index():
 
 
 def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+    return """Suggest three names for a subject that is taught in mars.
 
 Animal: Cat
 Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
@@ -31,5 +30,12 @@ Animal: Dog
 Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
 Animal: {}
 Names:""".format(
+        animal.capitalize()
+    )
+
+def generate_prompt2(animal):
+    return """How do animals reproduce
+     Animal: {}
+     """.format(
         animal.capitalize()
     )
